5e361aae68f9cca3f4b42b0e113e2b47166eaa9b
Artem Kislov <akislov.im@gmail.com>
2022-09-18 13:41:18 +0300

    extend search linear find test

--- packages/algorithms/src/search/linear/linear.test.ts
+++ packages/algorithms/src/search/linear/linear.test.ts
@@ -9,13 +9,15 @@ test('Linear search', () => {
 })
 
 test('Linear find', () => {
-    const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    const arrayPrimitives = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
     const arrayObjects = [
         { id: '1', value: 'a' },
         { id: '2', value: 'b' },
         { id: '3', value: 'c' },
         { id: '4', value: 'd' },
     ];
+    expect(find(arrayPrimitives, (v) => v === 1)).toBe(1);
+    expect(find(arrayPrimitives, (v) => v === 10)).toBeNull();
     expect(find(arrayObjects, (v) => v.id === '1')).toStrictEqual({ id: '1', value: 'a' });
     expect(find(arrayObjects, (v) => v.id === '5')).toBeNull();
 })
